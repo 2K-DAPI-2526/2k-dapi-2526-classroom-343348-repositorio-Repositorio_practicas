81ba5450346b381f154959cc4b7e8479f6f50045
jgrandamel-dev <jgrandamel@educacion.navarra.es>
2025-10-25 13:04:49 +0200

    Update Ejercicio4.py

--- EJERCICIOS_ENTREGA/Ejercicio4.py
+++ EJERCICIOS_ENTREGA/Ejercicio4.py
@@ -1,10 +1,3 @@
 #Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
 
 
-num = int(input("Introduce un número entero: "))
-if num % 2 == 0:
-    print("El número es par")  
-else:
-    print("El número es impar")
-# --- IGNORE ---
-

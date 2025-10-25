046191eb287bc2d5e0b18eb9bfb2ecc432323983
jgrandamel-dev <jgrandamel@educacion.navarra.es>
2025-10-25 13:38:14 +0200

    Update Ejercicio1.py

--- Ejercicios entrega/UT2/Ejercicio1.py
+++ Ejercicios entrega/UT2/Ejercicio1.py
@@ -2,9 +2,4 @@
 #Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no
 
 
-Edad = int(input("Introduce tu edad: "))
-if Edad >= 18:
-    print("Eres mayor de edad") 
-else:
-    print("Eres menor de edad")
-# --- IGNORE ---
+

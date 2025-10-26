db2331c3fea1c89b3cf3f43be810e616236eef87
jgrandamel <jgrandamel@educacion.navarra.es>
2025-10-26 20:43:54 +0100

    a

--- AnadirDuda.py
+++
@@ -1,6 +0,0 @@
-#Este archivo servirá para añadir un nuevo hilo de dudas en el archivo Dudas.txt
-ruta_dudas = "Dudas.txt"
-nueva_duda = input("Ingrese la nueva duda: ")
-with open(ruta_dudas, "a", encoding="utf-8") as archivo:
-    archivo.write(nueva_duda + "\n")
-print("Duda añadida correctamente.")
